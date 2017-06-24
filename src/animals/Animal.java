6ed9cb130348ed20139978a3e5c7b5abb2f9c022
stephane <stephanecharite@gmail.com>
2017-06-24 18:28:46 -0400

    Created first repo

---
+++ src/animals/Animal.java
@@ -0,0 +1,13 @@
+package animals;
+
+/**
+ * Created by Crown on 6/24/2017.
+ */
+public interface Animal {
+
+    public String getName();
+    public String getSpecies();
+    public int getLifeSpan();
+    public String [] getCanEat();
+
+}
